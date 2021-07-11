838eda0442d1b766ba18334e866a2b378fb456ed
Nadav Yihie <nadavyihye@gmail.com>
2021-07-11 18:12:46 +0300

    add game+css

--- 8.frontend-react/managerapp/src/Pages/HomePage/HomePage.jsx
+++ 8.frontend-react/managerapp/src/Pages/HomePage/HomePage.jsx
@@ -1,5 +1,7 @@
-import React from 'react';
+
+import React, { useState } from 'react';
 import './css/homePage-style.css'
+import { MdAccountCircle } from "react-icons/md";
 import {
     BrowserRouter as Router,
     Switch,
@@ -8,30 +10,38 @@ import {
     Link,
   } from "react-router-dom";
 function HomePage(props) {
+  const [chosenOption,setChosenOption]=useState("home");
     const logOut = () => {
         localStorage.removeItem("token");
         window.location.href='/na';
       };
+      const highlightOption=(option)=>{
+        setChosenOption(option);
+      }
 
     // console.log(props.userDetails);
     return (
         <div className="homepage">
-            
+            <div className="titleDiv">
+            <span className="companyTitle">{props.userDetails[0].companyName}</span>
+            </div>
            
         <div className="topNav">
-        {/* <Link to="/users" className="topNavItem" >users</Link> */}
-        {<Link className="topNavItem" to="/users">users</Link>}
-
-        <Link to="/clients" className="topNavItem">Clients </Link>
-        <div className="topNavItem">option </div>
+        {<Link className={chosenOption=='home'?"topNavItemChosen":"topNavItem"} to="/" onClick={()=>{highlightOption("home")}}>Home</Link>}
+        {<Link className={chosenOption=='users'?"topNavItemChosen":"topNavItem"} to="/users" onClick={()=>{highlightOption("users")}}>Users</Link>}
+        {<Link className={chosenOption=='clients'?"topNavItemChosen":"topNavItem"} to="/clients" onClick={()=>{highlightOption("clients")}}>Clients</Link>}
+        {<Link className={chosenOption=='Rentals'?"topNavItemChosen":"topNavItem"} to="/rentals" onClick={()=>{highlightOption("Rentals")}}>Rentals</Link>}
+        {<Link className={chosenOption=='Games'?"topNavItemChosen":"topNavItem"} to="/games" onClick={()=>{highlightOption("Games")}}>Games</Link>}
         </div>
+
         <div className="logout" onClick={logOut}>
         Sign out
+      
       </div>
-      <div className="username">Hi, {props.userDetails[0].fullName}</div>
+      <div className="userName"><MdAccountCircle className="userIcon"/>
+         {props.userDetails[0].fullName}
+      </div>
         </div>
-
-        
     );
 }
 
